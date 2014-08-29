266ad890c9d0d1880434ef0dea772dad22308538
Thor Pedersen <technostamper@hotmail.com>
2014-08-29 11:18:35 +0200

    Added exception to Age setter and implemented unit test

--- ClassLibrary1/Person.cs
+++ ClassLibrary1/Person.cs
@@ -9,22 +9,32 @@ namespace ClassLibrary1
     public class Person
     {
         private string _name;
-        private double _age;
+        private decimal _age;
         public string Name
         {
             get { return _name; }
             set
             {
-                if (string.IsNullOrWhiteSpace(value))
-                    throw new ArgumentNullException();
-                _name = value;
+               if (string.IsNullOrWhiteSpace(value))
+               {
+                  throw new ArgumentNullException();
+               }
+               _name = value;
             }
+           
         }
 
-        public double Age
+        public decimal Age
         {
-            get { return _age; }
-            set { _age = value; }
+           get { return _age; }
+           set
+           {
+              if(value < 0)
+              { 
+              throw new AgeException();
+              }
+              _age = value;
+           }
         }
 
         public Person()
@@ -39,5 +49,39 @@ namespace ClassLibrary1
         {
             return _age >= 18;
         }
+
+
+       public bool Equals(Person other)
+       {
+          if (ReferenceEquals(null, other)) return false;
+          if (ReferenceEquals(this, other)) return true;
+          return string.Equals(_name, other._name) && _age == other._age;
+       }
+
+       public override int GetHashCode()
+       {
+          unchecked
+          {
+             return ((_name != null ? _name.GetHashCode() : 0)*397) ^ _age.GetHashCode();
+          }
+       }
+
+       public override bool Equals(object obj)
+        {
+          if (ReferenceEquals(null, obj)) return false;
+          if (ReferenceEquals(this, obj)) return true;
+          if (obj.GetType() != this.GetType()) return false;
+          return Equals((Person) obj);
+        }
+
+       public static bool operator ==(Person left, Person right)
+       {
+          return Equals(left, right);
+       }
+
+       public static bool operator !=(Person left, Person right)
+       {
+          return !Equals(left, right);
+       }
     } 
 }
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -23,19 +23,73 @@ namespace UnitTestProject1
         public void TestAdult()
         {
             // Under 18 should return false
-            _p.Age = 17.99;
+            _p.Age = 17;
             bool result = _p.isAdult();
             Assert.IsFalse(result);
 
             // Equal to 18 should return true
-            _p.Age = 18.00;
+            _p.Age = 18;
             result = _p.isAdult();
             Assert.IsTrue(result);
 
             // Over 18 should return true
-            _p.Age = 18.01;
+            _p.Age = 19;
             result = _p.isAdult();
             Assert.IsTrue(result);
         }
+
+       [TestMethod]
+       public void TestEquality()
+       {
+          Person p = new Person();
+          p.Name = "Børge";
+          p.Age = 24;
+
+          Person otherP = new Person();
+          otherP.Name = "Børge";
+          otherP.Age = 24;
+
+          Assert.AreEqual(p, otherP);
+       }
+
+       [TestMethod]
+       public void TestInequality()
+       {
+          Person p = new Person();
+          p.Name = "Børgen";
+          p.Age = 24;
+
+          Person otherP = new Person();
+          otherP.Name = "Børge";
+          otherP.Age = 24;
+
+          Assert.AreNotEqual(p, otherP);
+       }
+
+       [TestMethod]
+       public void TestAgeException()
+       {
+          Person p = new Person();
+          p.Name = "Børgen";
+
+
+          try
+          {
+             p.Age = -26;
+             Assert.Fail();
+          }
+          catch (AgeException ae)
+          {
+             Assert.AreEqual("Alder for lav", ae.Message);
+          }
+          catch (Exception e)
+          {
+             Assert.Fail();
+          }
+          
+
+       }
+
+
     }
 }
